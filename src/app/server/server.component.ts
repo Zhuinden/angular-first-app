57e8100de2b20ffcb0936c8af07d1398210c8190
Zhuinden <zhuinden@gmail.com>
2017-08-10 16:23:25 +0200

    replace ngStyle with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
     serverId = 10;
@@ -14,9 +15,5 @@ export class ServerComponent {
 
     getServerStatus() {
         return this.serverStatus;
-    }
-
-    getColor() {
-        return this.serverStatus === 'online' ? 'palegreen' : 'mistyrose';
     }
 }
