633ee3245509133a07a1387f88f9c352f1f5fc37
Zhuinden <zhuinden@gmail.com>
2017-08-14 14:05:31 +0200

    add @Input() to highlight directive

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,24 +1,28 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
     selector: '[highlight]'
 })
 export class HighlightDirective implements OnInit {
-    @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+    @Input() defaultColor: string = 'transparent';
+    @Input() highlightColor: string = 'blue';
+
+    @HostBinding('style.backgroundColor') backgroundColor: string;
 
     constructor(private elRef: ElementRef, private renderer: Renderer2) { // unused
     }
 
     ngOnInit(): void {
+        this.backgroundColor = this.defaultColor;
     }
 
     @HostListener('mouseenter')
     mouseenter(eventData: Event) {
-        this.backgroundColor = 'deepskyblue';
+        this.backgroundColor = this.highlightColor;
     }
 
     @HostListener('mouseleave')
     mouseleave(eventData: Event) {
-        this.backgroundColor = 'transparent';
+        this.backgroundColor = this.defaultColor;
     }
 }
